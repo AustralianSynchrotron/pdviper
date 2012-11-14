e1a088ddaa9fc796a709c7e65217a580f71449d7
Gary Ruben <gary.ruben@gmail.com>
2012-11-15 10:49:38 +1100

    Changed start_pdviper.sh to pick up the path on macs

--- start_pdviper.sh
+++ start_pdviper.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash                                                                     
+if [ `uname` == 'Darwin' ]
+then
+    if [ -f ~/.bash_profile ]
+    then
+        source ~/.bash_profile
+    fi
+fi
 echo "Starting PDViPeR..."
 python app.py
-
