6517ac704f734cc171c77f95a6bfefda0aeb3f83
Ulrich Felzmann <ulrich.felzmann@gmail.com>
2015-05-15 13:18:42 +1000

    Fixed issue for 64bit Mac

--- setup_fortran_bin.py
+++ setup_fortran_bin.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-import os, stat, sys, shutil,platform, subprocess, struct
+
+import os, sys, shutil, platform, struct
+
 home = 'http://synchrotron.org.au/pdviper'
 print 70*'*'
 print 'Checking python packages...',
+
 missing = []
 for pkg in [
         'numpy',
@@ -23,7 +26,8 @@ for pkg in [
         missing.append(pkg)
 
 if missing:
-    print """Sorry, this version of Python cannot be used
+    print """
+Sorry, this version of Python cannot be used
 for PDViPeR. It is missing the following package(s):
 \t""",
     for pkg in missing:
@@ -36,31 +40,26 @@ if not os.path.exists('bin'):
     os.mkdir('bin')
     open(os.path.join('bin', '__init__.py'), 'w')
 
-
+src = None
 if sys.platform.startswith('linux'):
-    #if platform.processor().find('86') <= -1:
-    #    ans = raw_input("Note, GSAS requires an Intel-compatible processor and 32-bit"
-    #                    "libraries.\nAre you sure want to continue? [Yes]/no: ")
-    #    if ans.lower().fin('no') > -1: sys.exit()
     src = 'binlinux64-2.7'
-    files = os.listdir('binlinux64-2.7')
-    print 'Linux, Intel-compatible'
-elif sys.platform == "darwin" and platform.processor() == 'i386':
+elif sys.platform == 'darwin' and platform.machine() == 'x86_64':
+    src = 'binmac64-2.7'
+elif sys.platform == 'darwin' and platform.machine() == 'i386':
     src = 'binmac2.7'
-    files = os.listdir('binmac2.7')
-    print 'Mac OS X, Intel-compatible'
-elif sys.platform == "win32":
+elif sys.platform == 'win32':
     if struct.calcsize("P")*8 == 64:
         src = 'binwin64-2.7'
-        files = os.listdir('binwin64-2.7')
     else:
         src = 'binwin2.7'
-        files = os.listdir('binwin2.7')
-elif sys.platform == "darwin":
+elif sys.platform == 'darwin':
     print 'Mac OS X, PowerPC -- you will need to run f2py on fsource files'
 else:
     print 'Unidentifed platform -- you may need to run f2py on fsource files'
-if len(files) > 0:
+
+if src:
+    files = os.listdir(src)
     for f in files:
         fullfilename = os.path.join(src, f)
         shutil.copy(fullfilename, 'bin')
+    print src
