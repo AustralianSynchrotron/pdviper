deeab152bea9e05fc85eff94ba784ca15effc541
RobbieClarken <robbie.clarken@gmail.com>
2017-10-11 12:54:29 +1100

    Use a splitter so the plot window can be resized

--- pdviper/gui/main_window.py
+++ pdviper/gui/main_window.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSplitter
+from PyQt5.QtCore import Qt
 
 from .controls_panel import ControlsPanel
 from .plot_window import PlotWindow
@@ -8,20 +9,19 @@ class MainWindow(QWidget):
     def __init__(self, *, data_manager):
         super().__init__()
         self.data_manager = data_manager
+
         self.setWindowTitle('PDViPeR')
-        mainLayout = QHBoxLayout(self)
 
         self.controlsPanel = ControlsPanel(data_manager=self.data_manager)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(1)
-        self.controlsPanel.setSizePolicy(sizePolicy)
-
         self.plotWindow = PlotWindow(data_manager=self.data_manager)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(1)
-        self.plotWindow.setSizePolicy(sizePolicy)
 
-        mainLayout.addWidget(self.controlsPanel)
-        mainLayout.addWidget(self.plotWindow)
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(self.controlsPanel)
+        splitter.addWidget(self.plotWindow)
+
+        mainLayout = QHBoxLayout(self)
+        mainLayout.addWidget(splitter)
         self.setLayout(mainLayout)
+
         self.setGeometry(300, 300, 800, 600)
+        splitter.setSizes([350, 350])
