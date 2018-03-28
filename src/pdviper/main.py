069eb7ed91e881b440f1d967fb83b9fa026a1948
RobbieClarken <robbie.clarken@gmail.com>
2018-03-29 10:39:37 +1100

    Enable app to be closed from commandline with ctrl-c

--- src/pdviper/main.py
+++ src/pdviper/main.py
@@ -1,8 +1,19 @@
-from PyQt5.QtWidgets import QApplication
 import sys
+import signal
+
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtCore import QTimer
 
 from .gui import MainWindow
 from .data_manager import DataManager
+
+
+def _close_on_ctrl_c(app):
+    signal.signal(signal.SIGINT, lambda *_: app.quit())
+    # allow the python interpreter to be called so signals will be processed:
+    timer = QTimer(app)
+    timer.start(100)
+    timer.timeout.connect(lambda: None)
 
 
 def main():
@@ -11,4 +22,5 @@ def main():
     main_window = MainWindow(data_manager=data_manager)
     main_window.show()
     data_manager.load(sys.argv[1:])
+    _close_on_ctrl_c(app)
     app.exec_()
